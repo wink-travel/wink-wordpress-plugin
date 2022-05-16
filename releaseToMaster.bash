926a685e49f93d8616e464a6b542e658192f0afc
Bjorn Harvold <bjorn@harvold.com>
2022-05-16 11:49:58 +0700

    build: :construction_worker: moved changelog out of README.md to CHANGELOG.md
    README.txt stays the same as WordPress needs it

--- releaseToMaster.bash
+++ releaseToMaster.bash
@@ -11,6 +11,9 @@ echo "Releasing new version of iko-travel-affiliate WordPress plugin with git fl
 echo "Enter version number. E.g. 1.2.3";
 
 read versionNumber
+
+git cliff --unreleased --tag versionNumber --sort newest --prepend CHANGELOG.md
+
 versionNumber="v${versionNumber}";
 
 echo "Committing version changes for $versionNumber"
